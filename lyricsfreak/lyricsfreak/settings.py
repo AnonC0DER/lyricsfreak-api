cadc57e07af243f52dd7e868c6f5b6427b4736f1
Hesam N <AnonCODER@tutanota.com>
2022-02-09 15:53:39 +0330

    edited

--- lyricsfreak/lyricsfreak/settings.py
+++ lyricsfreak/lyricsfreak/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import os
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
