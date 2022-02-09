e845be772b26248c78e5eba750c945f2225c9f0b
Hesam N <AnonCODER@tutanota.com>
2022-02-09 15:00:25 +0330

    Update settings.py

--- lyricsfreak/lyricsfreak/settings.py
+++ lyricsfreak/lyricsfreak/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sdct$zr)o(=$sisoe1#y#vnw!j-*t0!&nl15h^8cth^z5igi!4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
